80e6e41d6cae78c4e97cec48407c16d7ccca7b66
Giuliano Trujillo <Neotel@GDTP>
2025-04-04 20:19:08 -0500

    Agregar archivos de proyecto.

---
+++ RepasoWeek_CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RepasoWeek_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer núumero:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo núumero:");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer núumero:");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 > n2 && n2 < n3)
+            {
+                Console.WriteLine("El mayor número es el número: " + n1);
+            }
+            else if (n2 > n1 && n1 < n3) { 
+            Console.WriteLine("El mayor número es el número: " + n2);
+             }
+            else{
+               Console.WriteLine("El mayor número es el número: " + n3);}
+        }
+    }
+}
